8c14a15074f5dca02365e58df2c9479748691b25
Wallace-Santana <wallace-santana@outlook.com.br>
2025-05-26 10:28:28 -0300

    cadastrando log no banco

--- src/controllers/delivery-logs-controller.ts
+++ src/controllers/delivery-logs-controller.ts
@@ -1,8 +1,38 @@
 import { Response , Request } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod"
+import { AppError } from "@/utils/AppError";
 
 class DeliveryLogsController {
-    async create(requeste:Request, response: Response){
-        return response.json({message: "ok"})
+    async create(request:Request, response: Response){
+        const bodySchema = z.object({
+            delivery_id: z.string().uuid(),
+            description: z.string()
+        })
+
+        const {delivery_id, description} = bodySchema.parse(request.body)
+
+        const delivery = await prisma.delivery.findUnique({
+            where: {id: delivery_id}
+        })
+
+        if(!delivery){
+            throw new AppError("Delivery not found", 404)
+        }
+
+        if(delivery.status === "processing"){
+            throw new AppError("Change status to shipped", 404)
+        }
+
+        await prisma.deliveryLog.create({
+            data:{
+                deliveryId: delivery_id,
+                description
+            }
+        })
+
+
+        return response.status(201).json()
     }
 }
 
